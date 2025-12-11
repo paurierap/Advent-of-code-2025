eecf3f97fb89a1d3a94b25ed5bb6a46b29ffb507
Pau Riera <pau.riera.p@gmail.com>
2025-12-11 17:39:32 +0100

    Day 9

---
+++ day-9/main.cpp
@@ -0,0 +1,71 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+
+bool onSegment(const std::vector<int>&, const std::vector<int>&, const std::vector<int>&);
+bool isPointEnclosed(const std::vector<int>&, const std::vector<std::vector<int>>&);
+int orientation(const std::vector<int>&, const std::vector<int>&, const std::vector<int>&);
+bool doIntersect(const std::vector<std::vector<int>>&, const std::vector<std::vector<int>>&);
+bool isRectangleEnclosed(const std::vector<int>&, const std::vector<int>&, const std::vector<std::vector<int>>&);
+long long calculateArea(const std::vector<int>&, const std::vector<int>&);
+
+std::vector<std::vector<int>> getPolygon(std::ifstream& input)
+{
+    std::vector<std::vector<int>> polygon;
+    int x, y;
+    char comma;
+
+    while (input >> x >> comma >> y) polygon.push_back({x, y});
+
+    return polygon;
+};
+
+long long partI(const std::vector<std::vector<int>>& polygon)
+{
+    long long largestArea = 0;
+    for (int i = 0; i < polygon.size(); ++i)
+    {
+        for (int j = i + 1; j < polygon.size(); ++j) largestArea = std::max(largestArea, calculateArea(polygon[i], polygon[j]));
+    }
+
+    return largestArea;
+}
+
+long long partII(const std::vector<std::vector<int>>& polygon)
+{
+    long long largestArea = 0;
+    int n = polygon.size();
+    
+    std::cout << "Progress counter on corners visited for Part II: \n";
+    for (int i = 0; i < n; ++i)
+    {
+        std::cout << "Visited " << i << " of " << n << " corners \n";
+        for (int j = i + 1; j < n; ++j) 
+        {
+            int area = calculateArea(polygon[i], polygon[j]);
+
+            // Prune by area to avoid innecessary calculations
+            if (area > largestArea)
+            {
+                if (isRectangleEnclosed(polygon[i], polygon[j], polygon)) largestArea = area;
+            }
+        }
+    }
+
+    return largestArea;
+}
+
+int main(int argc, char* argv[])
+{
+    std::ifstream input(argv[1]);
+    std::vector<std::vector<int>> polygon = getPolygon(input);
+
+    std::cout << "The largest area of any rectangle we can make in Part I is " << partI(polygon) << "\n";
+
+    long long largestRectanglePartII = partII(polygon);
+    std::cout << "The largest area of any rectangle we can make in Part II is " << largestRectanglePartII << "\n";
+
+    return EXIT_SUCCESS;
+}
